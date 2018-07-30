6b71ce88f6a1c5b23c5e440721496ce3238d1293
Dmitriy_Dubravin <dmitry.dubravin@bigdropinc.com>
2018-07-30 15:29:20 +0300

    removed jQuery dependency

--- parallax.js
+++ parallax.js
@@ -5,13 +5,14 @@ export default class Parallax {
      * @param options(Object)
      */
     constructor(options) {
-        this.parallaxSection = $(options.parallaxSection);
-        this.elementsToMove = $(options.elementsToMove);
-        this.speedAttrName = 'data-speed';
+        this.parallaxSection = document.querySelector(options.parallaxSection);
+        this.elementsToMove = [...document.querySelectorAll(options.elementsToMove)];
+        this.speedAttrName = "data-speed";
         this.mobileWidth = options.breakpoint || 1200;
         this.elementsOffset = options.elementsOffset || 0;
         this.screenDivider = options.screenDivider || 2.5;
-        this.windowHeight = $(window).innerHeight();
+        this.window = window;
+        this.windowHeight = this.window.innerHeight;
 
         this.scrollEvent = this._scrollEvent.bind(this);
         this.parallaxInit = this._parallaxInit.bind(this);
@@ -20,48 +21,56 @@ export default class Parallax {
     }
 
     _scrollEvent() {
-        const scrollTop = $(window).scrollTop();
-        const sectionOffsetTop = this.parallaxSection.offset().top;
+        const scrollTop = this.window.pageYOffset;
+        const sectionOffsetTop = this.parallaxSection.offsetTop;
         const sectionIsVisible = scrollTop + this.windowHeight / this.screenDivider > sectionOffsetTop;
 
         if (sectionIsVisible) {
-            this.elementsToMove.each((index, elem) => {
-                const elemSpeed = $(elem).attr(this.speedAttrName) ? Number($(elem).attr(this.speedAttrName)) : 8;
+            this.elementsToMove.forEach(elem => {
+
+                const elemSpeed = elem.getAttribute(this.speedAttrName) ? Number(elem.getAttribute(this.speedAttrName)) : 8;
                 let transformPosition;
 
                 if(this.elementsOffset !== 0 ){
                     transformPosition = this.elementsOffset + -((scrollTop + this.windowHeight / this.screenDivider - sectionOffsetTop) / 10 * elemSpeed);
                 }
                 else{
-                    transformPosition = -((scrollTop / this.screenDivider) / 10 * elemSpeed)
+                    transformPosition = -((scrollTop / this.screenDivider) / 10 * elemSpeed);
                 }
 
-                $(elem).css({
-                    transform: `translateY(${transformPosition}px)`
-                })
-            })
+                elem.style.transform = `translateY(${transformPosition}px)`;
+            });
         }
     }
 
     _parallaxInit() {
-        const mobile = $(window).innerWidth() < this.mobileWidth;
+        const mobile = document.documentElement.clientWidth < this.mobileWidth;
 
         if (mobile) {
-            $(window).on('scroll', function () {
-                return false
+            this.window.addEventListener("scroll", function () {
+                return false;
             });
-            $(window).off('scroll');
+            this.window.removeEventListener("scroll", this.scrollEvent);
         } else {
-            this.elementsToMove.each((index, elem) => {
-                $(elem).css({
-                    transform: `translateY(${this.elementsOffset}px)`
-                })
+            this.elementsToMove.forEach(elem => {
+                elem.style.transform = `translateY(${this.elementsOffset}px)`;
             });
-            $(window).on('scroll', this.scrollEvent);
+            this.window.addEventListener("scroll", this.scrollEvent);
         }
     }
 
+    addEventsListener(element, eventsNames, listener) {
+        for (
+            let  i = 0,
+                events = eventsNames.split(" "),
+                length = events.length;
+            i < length;
+            i++
+        ) {
+            element.addEventListener(events[i], listener, false);
+        }
+    }
     _init() {
-        $(window).on('load resize orientationchange', this.parallaxInit);
+        this.addEventsListener(this.window, "load resize orientationchange", this.parallaxInit);
     }
 }
